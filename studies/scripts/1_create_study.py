aec8c63ab1b6fb45f5b8e176f82e3fdf5486fa35
andrew960 <andrew960@hotmail.it>
2025-11-17 16:13:06 +0100

    updating to 2025

--- studies/scripts/1_create_study.py
+++ studies/scripts/1_create_study.py
@@ -57,7 +57,7 @@ d_config_mad = {"beam_config": {"lhcb1": {}, "lhcb2": {}}, "links": {}}
 
 ### For run III ions
 d_config_mad["links"]["acc-models-lhc"] = "/afs/cern.ch/eng/lhc/optics"
-d_config_mad["optics_file"] = "acc-models-lhc/runIII/RunIII_dev/ION_2024/opticsfile.21"
+d_config_mad["optics_file"] = "acc-models-lhc/runIII/RunIII_dev/ION_2025/opticsfile.21"
 d_config_mad["ver_hllhc_optics"] = None
 d_config_mad["ver_lhc_run"] = 3.0
 
