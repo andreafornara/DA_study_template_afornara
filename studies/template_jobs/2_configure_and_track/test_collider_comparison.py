540e38a2026525150a4a63b92378c49728f23081
andrew960 <andrew960@hotmail.it>
2026-01-13 14:53:31 +0100

    added tracking study

--- studies/template_jobs/2_configure_and_track/test_collider_comparison.py
+++ studies/template_jobs/2_configure_and_track/test_collider_comparison.py
@@ -107,24 +107,71 @@ plt.suptitle('Beta functions', fontsize=fs)
 plt.tight_layout()
 
 # %%
-# Define a particle and track for 10 turns
+# Check beam-beam lens installation
+elements_b1 = env['lhcb1'].element_names
+elements_b2 = env['lhcb2'].element_names
+
+hobb_b1 = [elem for elem in elements_b1 if elem.startswith('bb_ho')]
+lrbb_b1 = [elem for elem in elements_b1 if elem.startswith('bb_lr')]
+hobb_b2 = [elem for elem in elements_b2 if elem.startswith('bb_ho')]
+lrbb_b2 = [elem for elem in elements_b2 if elem.startswith('bb_lr')]
+
+for beam_name, hobb, lrbb in [('Beam 1', hobb_b1, lrbb_b1), ('Beam 2', hobb_b2, lrbb_b2)]:
+    print(f"\n{beam_name}:")
+    for ip in ['1', '2', '5', '8']:
+        n_ho_left = sum(1 for elem in hobb if f'.l{ip}' in elem)
+        n_ho_center = sum(1 for elem in hobb if f'.c{ip}' in elem)
+        n_ho_right = sum(1 for elem in hobb if f'.r{ip}' in elem)
+        n_lr_left = sum(1 for elem in lrbb if f'.l{ip}' in elem)
+        n_lr_right = sum(1 for elem in lrbb if f'.r{ip}' in elem)
+        print(f"  IP{ip}: HO({n_ho_left}L + {n_ho_center}C + {n_ho_right}R), LR({n_lr_left}L + {n_lr_right}R)")
+
+# %%
+# Define particles and track for 100 turns
 nemitt_x = 2.5e-6
 nemitt_y = 2.5e-6
 delta_max = 27.e-5
 
+sigma_min = 0.1
+sigma_max = 8.0
+n_particles = 3
+
+x_norm = np.linspace(sigma_min, sigma_max, n_particles)
+y_norm = np.linspace(sigma_min, sigma_max, n_particles)
+amplitudes = np.sqrt(x_norm**2 + y_norm**2)
+
 particles = env['lhcb1'].build_particles(
-    x_norm=1.0,
-    y_norm=1.0,
+    x_norm=x_norm,
+    y_norm=y_norm,
     delta=delta_max,
     nemitt_x=nemitt_x,
     nemitt_y=nemitt_y
 )
 
-env['lhcb1'].track(particles, num_turns=10, turn_by_turn_monitor=True)
-
-print(f"\nParticle tracking completed for 10 turns")
-print(f"Final state: {particles.state[0]}")
-print(f"Final x: {particles.x[0]:.6e} m")
-print(f"Final y: {particles.y[0]:.6e} m")
+env['lhcb1'].track(particles, num_turns=2000, turn_by_turn_monitor=True)
+xs = env['lhcb1'].record_last_track.x
+ys = env['lhcb1'].record_last_track.y
+pxs = env['lhcb1'].record_last_track.px
+pys = env['lhcb1'].record_last_track.py
 
 # %%
+fig, ax = plt.subplots(2, 1, figsize=(15, 10))
+
+for i in range(n_particles):
+    ax[0].scatter(xs[i, :], pxs[i, :], c=amplitudes[i]*np.ones(xs.shape[1]),
+                  cmap='viridis', vmin=amplitudes.min(), vmax=amplitudes.max(), s=10)
+    ax[1].scatter(ys[i, :], pys[i, :], c=amplitudes[i]*np.ones(ys.shape[1]),
+                  cmap='viridis', vmin=amplitudes.min(), vmax=amplitudes.max(), s=10)
+
+ax[0].set_ylabel('px', fontsize=fs)
+ax[0].set_xlabel('x [m]', fontsize=fs)
+ax[0].grid(True)
+ax[0].tick_params(axis='both', labelsize=fs)
+
+ax[1].set_ylabel('py', fontsize=fs)
+ax[1].set_xlabel('y [m]', fontsize=fs)
+ax[1].grid(True)
+ax[1].tick_params(axis='both', labelsize=fs)
+
+plt.suptitle('Phase Space', fontsize=fs)
+plt.tight_layout()
